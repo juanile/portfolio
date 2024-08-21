261a8ceed6e9cbd3978d2ff9aea069876a9fdfa4
juanile <juanile97@gmail.com>
2024-08-21 20:23:27 -0300

    cambios en index, js y css para mejorar la carga de la pagina

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Ocultar el contenido principal al cargar la página
+  document.getElementById('mainContent').classList.add('hidden');
+
+  // Agregar el evento al botón de "Entrar"
+  document.getElementById('enter-site-btn').addEventListener('click', () => {
+    // Desplazarse hacia la parte superior de la página
+    window.scrollTo({ top: 0, behavior: 'auto' });
+
+    // Ocultar el overlay de introducción con una transición
+    const overlay = document.getElementById('intro-overlay');
+    overlay.classList.add('hidden');
+
+    // Mostrar el contenido principal
+    document.getElementById('mainContent').classList.remove('hidden');
+
+    // Esperar hasta que la transición termine para remover el overlay del DOM
+    setTimeout(() => {
+      overlay.style.display = 'none';
+    }, 1000); // Asegúrate de que este tiempo coincida con el tiempo de la transición en CSS
+  });
+});
+
 // Mostrar el botón solo cuando el usuario se desplaza hacia abajo
 window.onscroll = function() {
   const scrollToTopBtn = document.getElementById('scrollToTopBtn');
@@ -24,22 +47,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// intro
-
-document.getElementById('enter-site-btn').addEventListener('click', function () {
-  // Desplazarse hacia arriba
-  window.scrollTo({ top: 0, behavior: 'auto' });
 
 
-
-  const overlay = document.getElementById('intro-overlay');
-  overlay.classList.add('hidden');
-
-  // Esperar hasta que la transición termine para remover el overlay del DOM
-  setTimeout(() => {
-    overlay.style.display = 'none';
-  }, 1000); // El tiempo debe coincidir con el tiempo de la transición en CSS
-});
 
 
 
